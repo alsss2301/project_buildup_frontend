868f8a92c7e32e8c57d2e399c3ddcc1dfde6a1ec
Minseo Jeong <jeongminseo999@gmail.com>
2022-03-02 21:56:38 +0900

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
+import Login from "./components/Login.js";
 
 function App() {
   return (
     <Routes>
-      <Route path={`/`} exact element={<Home />} />
+      <Route path="/" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
     </Routes>
   );
 }
